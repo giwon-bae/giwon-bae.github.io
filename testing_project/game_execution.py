b3bac96129526a00eae93de2a350879af4225c65
giwon-bae <giwon.bae77@gmail.com>
2021-03-03 14:52:51 +0900

    .

--- testing_project/game_execution.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-subprocess.SW_HIDE = 1
-r = subprocess.Popen(r'D:\DevProg\HFC3.0\resource\Distrib-x64-2017-08-21\hfAnalyzer -o'+inputFileName, shell=True).wait()
-if r == 1: 
-    print('running error')
